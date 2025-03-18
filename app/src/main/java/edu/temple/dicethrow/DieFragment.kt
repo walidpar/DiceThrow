b3f1a5b6d4e79cbc249170feb37e3a5b98ae4cfe
Walid Parast <walidparast1@gmail.com>
2025-03-18 11:28:58 -0400

    Fixed bug not allowing die roll to hit 6, and allowed it to hit 0.

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -44,6 +44,6 @@ class DieFragment : Fragment() {
     }
 
     fun throwDie() {
-        dieTextView.text = Random.nextInt(dieSides).toString()
+        dieTextView.text = (Random.nextInt(dieSides) + 1).toString()
     }
 }
